89d1997c9235cfd3a429c0e2ec0320c1acf1a2fd
tomas-jacionis <jacionis.tomas@gmail.com>
2019-10-12 23:18:20 +0300

    Add files via upload
    Started project made around 30 % of project

---
+++ Tournament/Tournament/Form2.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Tournament
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4 Dalyviai",
+            "5 Dalyviai",
+            "6 Dalyviai",
+            "7 Dalyviai",
+            "8 Dalyviai",
+            "9 Dalyviai",
+            "10 Dalyvių",
+            "11 Dalyvių",
+            "12 Dalyvių",
+            "13 Dalyvių",
+            "14 Dalyvių",
+            "15 Dalyvių",
+            "16 Dalyvių",
+            "17 Dalyvių",
+            "18 Dalyvių",
+            "19 Dalyvių",
+            "20 Dalyvių",
+            "21 Dalyvis",
+            "22 Dalyviai",
+            "23 Dalyviai",
+            "24 Dalyviai",
+            "25 Dalyviai",
+            "26 Dalyviai",
+            "27 Dalyviai",
+            "28 Dalyviai",
+            "29 Dalyviai",
+            "30 Dalyvių",
+            "31 Dalyvis",
+            "32 Dalyviai"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tournament.Properties.Resources._27;
+            this.pictureBox1.Location = new System.Drawing.Point(328, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(869, 596);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 708);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Tournament/Tournament/Form2.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tournament
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+            
+
+            if (comboBox1.SelectedIndex.Equals(0))  {
+
+                pictureBox1.Image = Properties.Resources._4;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(1))
+            {
+
+                pictureBox1.Image = Properties.Resources._5;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(2))
+            {
+
+                pictureBox1.Image = Properties.Resources._6;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(3))
+            {
+
+                pictureBox1.Image = Properties.Resources._7;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(4))
+            {
+
+                pictureBox1.Image = Properties.Resources._8;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(5))
+            {
+
+                pictureBox1.Image = Properties.Resources._9;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(6))
+            {
+
+                pictureBox1.Image = Properties.Resources._10;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(7))
+            {
+
+                pictureBox1.Image = Properties.Resources._11;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(8))
+            {
+
+                pictureBox1.Image = Properties.Resources._12;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(9))
+            {
+
+                pictureBox1.Image = Properties.Resources._13;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(10))
+            {
+
+                pictureBox1.Image = Properties.Resources._14;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(11))
+            {
+
+                pictureBox1.Image = Properties.Resources._15;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(12))
+            {
+
+                pictureBox1.Image = Properties.Resources._16;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(13))
+            {
+
+                pictureBox1.Image = Properties.Resources._17;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(14))
+            {
+
+                pictureBox1.Image = Properties.Resources._18;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(15))
+            {
+
+                pictureBox1.Image = Properties.Resources._19;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(16))
+            {
+
+                pictureBox1.Image = Properties.Resources._20;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(17))
+            {
+
+                pictureBox1.Image = Properties.Resources._21;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(18))
+            {
+
+                pictureBox1.Image = Properties.Resources._22;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(19))
+            {
+
+                pictureBox1.Image = Properties.Resources._23;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(20))
+            {
+
+                pictureBox1.Image = Properties.Resources._24;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(21))
+            {
+
+                pictureBox1.Image = Properties.Resources._25;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(22))
+            {
+
+                pictureBox1.Image = Properties.Resources._26;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(23))
+            {
+
+                pictureBox1.Image = Properties.Resources._27;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(24))
+            {
+
+                pictureBox1.Image = Properties.Resources._28;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(25))
+            {
+
+                pictureBox1.Image = Properties.Resources._29;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(26))
+            {
+
+                pictureBox1.Image = Properties.Resources._30;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(27))
+            {
+
+                pictureBox1.Image = Properties.Resources._31;
+            }
+
+            if (comboBox1.SelectedIndex.Equals(28))
+            {
+
+                pictureBox1.Image = Properties.Resources._32;
+            }
+
+        }
+    }
+}
