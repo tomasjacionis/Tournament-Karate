ad4960953a6e6a379d116b198ad259e2f51066ee
TheWolfLT <TheWolfLT@DESKTOP-TF4OAHR>
2020-01-05 01:24:25 +0200

    All non visible labels added as comments

--- Tournament/Tournament/Form2.cs
+++ Tournament/Tournament/Form2.cs
@@ -312,6 +312,29 @@ namespace Tournament
                 label9.Location = new Point(436, 81);
                 label10.Location = new Point(1082, 191);
 
+                //label11.Visible = false;
+                //label12.Visible = false;
+                //label13.Visible = false;
+                //label14.Visible = false;
+                //label15.Visible = false;
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
+
 
             }
 
@@ -319,126 +342,419 @@ namespace Tournament
             {
 
                 pictureBox1.Image = Properties.Resources._11;
+
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                label8.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+                //  label11.Visible = true;
+
+                label1.Location = new Point(436, 389);
+                label2.Location = new Point(436, 543);
+                label3.Location = new Point(1082, 268);
+                label4.Location = new Point(986, 543);
+                label5.Location = new Point(354, 268);
+                label6.Location = new Point(986, 389);
+                label7.Location = new Point(963, 80);
+                label8.Location = new Point(354, 191);
+                label9.Location = new Point(436, 81);
+                label10.Location = new Point(1082, 191);
+                // label11.Location = new Point ();
+
+                //label12.Visible = false;
+                //label13.Visible = false;
+                //label14.Visible = false;
+                //label15.Visible = false;
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(8))
             {
 
                 pictureBox1.Image = Properties.Resources._12;
+
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                label8.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+               // label11.Visible = true;
+               // label12.Visible = true;
+
+                //label13.Visible = false;
+                //label14.Visible = false;
+                //label15.Visible = false;
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(9))
             {
 
                 pictureBox1.Image = Properties.Resources._13;
+
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                label8.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+                //label11.Visible = true;
+               // label12.Visible = true;
+               // label13.Visible = true;
+
+
+
+                //label14.Visible = false;
+                //label15.Visible = false;
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(10))
             {
 
                 pictureBox1.Image = Properties.Resources._14;
+
+                //label15.Visible = false;
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(11))
             {
 
                 pictureBox1.Image = Properties.Resources._15;
+
+                //label16.Visible = false;
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(12))
             {
 
                 pictureBox1.Image = Properties.Resources._16;
+
+                //label17.Visible = false;
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(13))
             {
 
                 pictureBox1.Image = Properties.Resources._17;
+
+                //label18.Visible = false;
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(14))
             {
 
                 pictureBox1.Image = Properties.Resources._18;
+
+                //label19.Visible = false;
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(15))
             {
 
                 pictureBox1.Image = Properties.Resources._19;
+
+                //label20.Visible = false;
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(16))
             {
 
                 pictureBox1.Image = Properties.Resources._20;
+
+                //label21.Visible = false;
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(17))
             {
 
                 pictureBox1.Image = Properties.Resources._21;
+
+                //label22.Visible = false;
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(18))
             {
 
                 pictureBox1.Image = Properties.Resources._22;
+
+                //label23.Visible = false;
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(19))
             {
 
                 pictureBox1.Image = Properties.Resources._23;
+
+                //label24.Visible = false;
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(20))
             {
 
                 pictureBox1.Image = Properties.Resources._24;
+
+                //label25.Visible = false;
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(21))
             {
 
                 pictureBox1.Image = Properties.Resources._25;
+
+                //label26.Visible = false;
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(22))
             {
 
                 pictureBox1.Image = Properties.Resources._26;
+
+                //label27.Visible = false;
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(23))
             {
 
                 pictureBox1.Image = Properties.Resources._27;
+
+                //label28.Visible = false;
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(24))
             {
 
                 pictureBox1.Image = Properties.Resources._28;
+
+                //label29.Visible = false;
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(25))
             {
 
                 pictureBox1.Image = Properties.Resources._29;
+
+                //label30.Visible = false;
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(26))
             {
 
                 pictureBox1.Image = Properties.Resources._30;
+
+                //label31.Visible = false;
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(27))
             {
 
                 pictureBox1.Image = Properties.Resources._31;
+
+                //label32.Visible = false;
             }
 
             if (comboBox1.SelectedIndex.Equals(28))
